bee7aa8f4d0ec8ee66356f604b7dc4a7d7fa4a93
GhianCo <ghiancorx@gmail.com>
2022-06-10 12:54:02 -0500

    cambios;

---
+++ gocancha_frontend/www/4376.f323f7c433b4ea41.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4376],{74376:(a,l,s)=>{s.r(l),s.d(l,{ion_text:()=>n});var o=s(32361),c=s(58686),e=s(67300);const n=class{constructor(t){(0,o.r)(this,t)}render(){const t=(0,c.b)(this);return(0,o.h)(o.H,{class:(0,e.c)(this.color,{[t]:!0})},(0,o.h)("slot",null))}};n.style=":host(.ion-color){color:var(--ion-color-base)}"}}]);
